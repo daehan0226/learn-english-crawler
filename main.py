c427c8eb988e1c110b6ba533703996de137423e5
daehan <dh16931@gmail.com>
2022-01-13 20:35:39 +0900

    style: clean code

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ config = simplejson.loads(json_config)
 def run_crawler(logging, env=None, keyword=None, keyword_type=None):
     check_run_cmd(env, keyword_type)
 
+    keywords = (
+        ApiHandler.get_keywords(env, keyword_type)
+        if keyword is None
+        else keyword.split(",")
+    )
+
     SiteCrawlers = [
         CrawlerCambridge,
         CrawlerCollins,
@@ -32,34 +38,25 @@ def run_crawler(logging, env=None, keyword=None, keyword_type=None):
         CrawlerOxford,
     ]
 
-    try:
-        keywords = (
-            ApiHandler.get_keywords(env, keyword_type)
-            if keyword is None
-            else keyword.split(",")
-        )
+    for keyword in keywords:
+        definitions = []
+        examples = []
 
-        for keyword in keywords:
-            definitions = []
-            examples = []
-            for SiteCrawler in SiteCrawlers:
-                crawler = SiteCrawler(logging)
-                crawler.keyword = keyword
-                logging.debug(
-                    f"site: {crawler.get_site()}, keyword : {crawler.keyword}"
-                )
-                crawler.set_url()
-                logging.debug(f"Loading {crawler.get_parse_url()} for {keyword}")
-                crawler.load()
-                crawler.parse()
+        for SiteCrawler in SiteCrawlers:
+            crawler = SiteCrawler(logging)
+            crawler.keyword = keyword
+            logging.debug(f"site: {crawler.get_site()}, keyword : {crawler.keyword}")
+            crawler.set_parse_url()
+            logging.debug(f"Loading {crawler.get_parse_url()} for {keyword}")
+            crawler.load()
+            crawler.parse()
 
-                definitions.extend(crawler.get_definitions())
-                examples.extend(crawler.get_examples())
+            definitions.extend(crawler.get_definitions())
+            examples.extend(crawler.get_examples())
 
-            print(definitions, examples)
-        logging.info("==Crawler finished==============")
-    except Exception as e:
-        print(e)
+    logging.info("==Crawler finished==============")
+
+    # Upload
 
 
 if __name__ == "__main__":
