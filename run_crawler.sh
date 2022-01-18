53766043540770d4077fe272ff16cac7eb95681a
Daehan <dh16931@gmail.com>
2022-01-18 10:08:02 +0000

    feat: add shell run kwargs

--- run_crawler.sh
+++ run_crawler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/learn-english-app/learn-english-crawler
 source /home/ubuntu/learn-english-app/learn-english-crawler/venv/bin/activate
-python ./main.py
+python ./main.py keyword=$1 keyword_type=$2 env=$3
